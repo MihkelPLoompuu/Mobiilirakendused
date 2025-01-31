b1bda7cb1475d68bc0d28ff42e69d4f34541e5bf
Opilane TTHK <158562003+MihkelPLoompuu@users.noreply.github.com>
2025-01-31 14:06:10 +0200

    tegime stiili
    tegime stiili

--- SaiCalc/SaiCalc/App.xaml.cs
+++ SaiCalc/SaiCalc/App.xaml.cs
@@ -4,6 +4,8 @@ using Microsoft.UI.Windowing;
 using Windows.Graphics;
 #endif
 
+using Views;
+
 namespace SaiCalc
 {
     public partial class App : Application
@@ -28,7 +30,7 @@ namespace SaiCalc
                     });
         #endif
 
-            MainPage = new AppShell();
+            MainPage = new CalculatorPage();
         }
     }
 }
--- SaiCalc/SaiCalc/ViewModels/CalculatorPageViewModel.cs
+++ SaiCalc/SaiCalc/ViewModels/CalculatorPageViewModel.cs
@@ -1,9 +1,127 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace ViewModels
 {
     [INotifyPropertyChanged]
     internal partial class CalculatorPageViewModel
     {
+        [ObservableProperty]
+        private string inputText = string.Empty;
+
+        [ObservableProperty]
+        private string calculatedResult = "0";
+
+        private bool isSciOpWaiting = false;
+
+        [RelayCommand]
+        private void Reset()
+        {            
+            CalculatedResult = "0";
+            InputText = string.Empty;
+            isSciOpWaiting = false;
+        }
+
+        [RelayCommand]
+        private void Calculate()
+        {
+            if (InputText.Length == 0)
+            {
+                return;
+            }
+
+            if(isSciOpWaiting)
+            {
+                InputText += ")";
+                isSciOpWaiting = false;
+            }
+
+            try {
+                var inputString = NormalizeInputString();
+                var expression = new NCalc.Expression(inputString);
+                var result = expression.Evaluate();
+
+                calculatedResult = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private string NormalizeInputString()
+        {
+            Dictionary<string, string> _opMapper = new()
+            {
+                {"×", "*"},
+                {"÷", "/"},
+                {"SQRT", "Sqrt"},
+                {"SIN", "Sin"},
+                {"COS", "Cos"},
+                {"TAN", "Tan"},
+                {"LOG", "Log"},
+                {"ASIN", "Asin"},
+                {"ACOS", "Acos"},
+                {"ATAN", "Atan"},
+                {"EXP", "Exp"},
+                {"LOG10", "Log10"},
+                {"POW", "Pow"},
+                {"TAN", "tan"},
+                {"ABS", "Abs"},
+                
+            };
+            
+            var retstring = InputText;
+            foreach (var key in _opMapper.Keys)
+            {
+                retstring = retstring.Replace(key, _opMapper[key]);
+            }
+            return retstring;
+        }
+
+        [RelayCommand]
+        private void Backspace()
+        {
+            if (InputText.Length > 0)
+            {
+                InputText = InputText.Substring(0, InputText.Length - 1);
+            }
+        }
+
+        [RelayCommand]
+        private void NumberInput(string key)
+        {
+            InputText += key;
+        }
+
+        [RelayCommand]
+        private void MathOperator(string op)
+        {
+            if (isSciOpWaiting)
+            {
+                InputText += ")";
+                isSciOpWaiting = false;
+            }
+
+            InputText += $" {op} ";
+        }
+
+        [RelayCommand]
+        private void RegionOperator(string op)
+        {
+            if (isSciOpWaiting)
+            {
+                InputText += ")";
+                isSciOpWaiting = false;
+            }
+            inputText += $" {op} ";
+        }
+
+        [RelayCommand]
+        private void ScientificOperator(string op)
+        {
+            InputText += $"{op}(";
+            isSciOpWaiting = true;
+        }
     }
 }
