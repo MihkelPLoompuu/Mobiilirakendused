9624e4202c8ddc78d1385a3536ff2b2eb57e1a6a
Opilane TTHK <158562003+MihkelPLoompuu@users.noreply.github.com>
2025-02-07 14:09:00 +0200

    Tegime StartPage
    Tegime StartPage

--- MAUI_Planec/MAUI_Planec/App.xaml.cs
+++ MAUI_Planec/MAUI_Planec/App.xaml.cs
@@ -3,6 +3,8 @@ using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using Windows.Graphics;
 #endif
+using Views;
+
 namespace MAUI_Planec
 {
     public partial class App : Application
@@ -13,20 +15,20 @@ namespace MAUI_Planec
         {
             InitializeComponent();
             #if WINDOWS
-                        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
-                        {
-                            var mauiWindow = handler.VirtualView;
-                            var nativeWindow = handler.PlatformView;
-                            nativeWindow.Activate();
-                            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
-                            WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
-                            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
-                            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+                Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+                {
+                  var mauiWindow = handler.VirtualView;
+                  var nativeWindow = handler.PlatformView;
+                  nativeWindow.Activate();
+                  IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+                  WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+                  AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+                  appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
 
-                        });
+                 });
             #endif
 
-            MainPage = new AppShell();
+            MainPage = new StartPage();
         }
     }
 }
