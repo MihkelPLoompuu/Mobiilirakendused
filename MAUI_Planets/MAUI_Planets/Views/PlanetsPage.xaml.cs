762c03e0d5c8c96101ff40a0f29a00cd5cfb998c
Opilane TTHK <158562003+MihkelPLoompuu@users.noreply.github.com>
2025-02-11 14:03:50 +0200

    Tegime PlanetsPage.xaml
    Tegime PlanetsPage.xaml

--- MAUI_Planets/MAUI_Planets/Views/PlanetsPage.xaml.cs
+++ MAUI_Planets/MAUI_Planets/Views/PlanetsPage.xaml.cs
@@ -23,4 +23,16 @@ public partial class PlanetsPage : ContentPage
 	{
 		await Navigation.PushAsync(new PlanetsDetailsPage(e.CurrentSelection.First() as Planet));
 	}
+
+	async void GridArea_Tapped(System.Object sender, System.EventArgs e)
+	{
+		await CloseMenu();
+	}
+
+	private async Task CloseMenu()
+	{
+		_ = MainContentGrid.FadeTo(1, AnimationDuration);
+		_ = MainContentGrid.ScaleTo(1, AnimationDuration);
+		await MainContentGrid.TranslateTo(0, 0, AnimationDuration, Easing.CubicIn);
+	}
 }
