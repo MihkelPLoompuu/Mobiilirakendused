4a182151f4a3b11b108ae4dbd8315c3e4366dde8
Opilane TTHK <158562003+MihkelPLoompuu@users.noreply.github.com>
2025-02-11 13:03:12 +0200

    Font
    Font

--- MAUI_Planec/MAUI_Planec/MauiProgram.cs
+++ MAUI_Planec/MAUI_Planec/MauiProgram.cs
@@ -11,8 +11,9 @@ namespace MAUI_Planec
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Monstserrat-Medium.ttf", "RegularFont");
+                    fonts.AddFont("Monstserrat-SemiBold.ttf", "MediumFont");
+                    fonts.AddFont("Monstserrat-Bold.ttf", "BoldFont");
                 });
 
 #if DEBUG
