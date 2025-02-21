86759aa887a7da0668511fb96dec7ec56157dcbb
Opilane TTHK <158562003+MihkelPLoompuu@users.noreply.github.com>
2025-02-21 12:50:32 +0200

    e
    e

--- MauiCRUD/MauiCRUD/ViewModels/ProductsViewModel.cs
+++ MauiCRUD/MauiCRUD/ViewModels/ProductsViewModel.cs
@@ -1,9 +1,24 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Data;
+using Models;
+using System.Collections.ObjectModel;
 
 
 namespace ViewModels
 {
     public partial class ProductsViewModel : ObservableObject
     {
+        private readonly DatabaseContext _context;
+
+        public ProductsViewModel(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [ObservableProperty]
+        private ObservableCollection<Product> _products = new();
+
+        [ObservableProperty]
+        private Product _
     }
 }
